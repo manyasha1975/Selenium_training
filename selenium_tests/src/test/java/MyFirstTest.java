868aef1b2eff683ad90d37c272cf30d6ea6e01c4
Maria Hudyakova <manyasha1975@rambler.ru>
2018-12-10 13:06:10 +0500

    Настроена инфраструктура и создан первый рабочий тест

---
+++ selenium_tests/src/test/java/MyFirstTest.java
@@ -0,0 +1,35 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+  private WebDriver driver;
+  private WebDriverWait wait;
+
+  @Before
+  public void start(){
+    driver = new ChromeDriver();
+    wait = new WebDriverWait(driver, 10);
+  }
+
+  @Test
+  public void testMyFirstTest() {
+    driver.get("http://google.com");
+    driver.findElement(By.name("q")).sendKeys("webdriver");
+    driver.findElement(By.name("btnK")).submit();
+    wait.until(titleIs("webdriver - Поиск в Google"));
+  }
+
+  @After
+  public void stop() {
+    driver.quit();
+    driver = null;
+  }
+}
