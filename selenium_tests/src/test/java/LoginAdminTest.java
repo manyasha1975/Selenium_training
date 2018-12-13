b361cf3e4073ba3d21430a960335f3b1a673083c
Maria Hudyakova <manyasha1975@rambler.ru>
2018-12-13 14:54:38 +0500

    Наведен порядок. Проект разложен по модулям

--- selenium_tests/src/test/java/LoginAdminTest.java
+++ selenium_tests/src/test/java/LoginAdminTest.java
@@ -2,10 +2,17 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class LoginAdminTest {
@@ -15,7 +22,16 @@ public class LoginAdminTest {
 
   @Before
   public void start(){
+    //run firefox in old mode
+    //DesiredCapabilities caps = new DesiredCapabilities();
+    //caps.setCapability(FirefoxDriver.MARIONETTE, false);
+    //if you want to use non-system folder for driver, set user folder with driver
+    //System.setProperty("webdriver.chrome.driver", "D:/Tools/chromedriver.exe");
     driver = new ChromeDriver();
+    //driver = new FirefoxDriver(new FirefoxBinary(new File("D:\\Tools\\ESR\\firefox.exe")),
+    //        new FirefoxProfile(), caps);
+    //driver = new InternetExplorerDriver();
+    System.out.println(((HasCapabilities) driver).getCapabilities());
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     wait = new WebDriverWait(driver, 10);
   }
