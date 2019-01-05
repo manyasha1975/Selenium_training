fe61c76452a36f8145067fbaa836b2f7679c66d6
Maria Hudyakova <manyasha1975@rambler.ru>
2019-01-05 18:41:00 +0500

    Реализован сценарий работы с корзиной

---
+++ selenium_tests/src/test/java/ru/mytest/litecart/tests/AdditionToBasketTests.java
@@ -0,0 +1,46 @@
+package ru.mytest.litecart.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.textToBe;
+
+public class AdditionToBasketTests extends TestBase {
+
+  @Test
+  public void testAdditionToBasket() {
+    //addition to cart
+    for (int i = 0; i < 3; i++) {
+      List<WebElement> ducks = app.driver.findElements(By.cssSelector("div.content .products li"));
+      ducks.get(0).findElement(By.cssSelector("a.link")).click();
+      String quantity = app.driver.findElement(By.cssSelector("div#cart .quantity"))
+              .getAttribute("textContent");
+      String newQuantity = Integer.toString(Integer.parseInt(quantity) + 1);
+      List<WebElement> select = app.driver.findElements(By.cssSelector("select"));
+      if (select.size() > 0) {
+        new Select(app.driver.findElement(By.cssSelector("select[name='options[Size]']")))
+                .selectByVisibleText("Small");
+      }
+      app.driver.findElement(By.cssSelector("button[name='add_cart_product']")).click();
+      app.wait.until(textToBe(By.cssSelector("div#cart .quantity"), newQuantity));
+      app.driver.findElement(By.cssSelector("a[href='http://localhost/litecart/en/']")).click();
+    }
+    //deletion from cart
+    app.driver.findElement(By.cssSelector("a[href='http://localhost/litecart/en/checkout']")).click();
+    for (int i = 0; i < 3; i++) {
+      List<WebElement> products = app.driver.findElements(By.cssSelector("table.dataTable tr td.item"));
+      app.driver.findElement(By.cssSelector("button[name='remove_cart_item']")).click();
+      app.wait.until(ExpectedConditions.stalenessOf(products.get(0)));
+    }
+    String noProducts = app.driver.findElement(By.cssSelector("div#checkout-cart-wrapper em"))
+            .getAttribute("textContent");
+    Assert.assertTrue(noProducts.equals("There are no items in your cart."));
+    app.driver.findElement(By.cssSelector("div#checkout-cart-wrapper a")).click();
+  }
+}
